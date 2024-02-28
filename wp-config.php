048521a0e491cad44f044c0aa2cb30886e145d3d
isai <acantuscr7@gmail.com>
2024-02-27 22:41:46 -0600

    innit

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'Programacion' );
+define( 'DB_NAME', 'programacion' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'admin' );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', '9012d31aff3277fcb373c5b8b863dd4d9800ea986c153c93' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'http://64.23.187.245/wordpress' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
