ff5f9305e93484148958b33322584f59fc31dba0
IsaiCantu7 <152317373+IsaiCantu7@users.noreply.github.com>
2024-03-13 23:55:56 -0600

    Add files via upload

--- php/act1/procesar_alta_alumno.php
+++ php/act1/procesar_alta_alumno.php
@@ -1,23 +1,17 @@
 <?php
-// incluir el archivo de conexión a la base de datos
 include 'db.php';
 
-// Iniciar o reanudar la sesión
 session_start();
 
 // Verificar si el formulario ya ha sido enviado
 if(isset($_SESSION['form_submitted']) && $_SESSION['form_submitted'] === true) {
-    // Si el formulario ya ha sido enviado, redirigir al formulario de alta de alumno
     header("Location: formulario_alta_alumno.php");
     exit();
 }
 
-// Verificar si se ha enviado el formulario de alta de alumno
 if(isset($_POST['alta_alumno'])) {
-    // Marcar el formulario como enviado
     $_SESSION['form_submitted'] = true;
 
-    // Obtener los datos del formulario
     $nombre = $_POST['nombre'];
     $apellido = $_POST['apellido'];
     $edad = $_POST['edad'];
@@ -32,7 +26,6 @@ if(isset($_POST['alta_alumno'])) {
         echo "Error al dar de alta al alumno: " . $conn->error;
     }
 
-    // Cerrar la conexión a la base de datos
     $conn->close();
 } else {
     // Si el formulario no se envió correctamente, redirigir al formulario de alta de alumno
